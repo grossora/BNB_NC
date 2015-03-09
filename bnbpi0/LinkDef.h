4595fe01c9d7ac5c3c0c4b1b7c48c31730a144ac
Ryan <grossora.neutrino@gmail.com>
2015-03-09 09:53:18 -0500

    Linkdef with more classes -ryan

--- bnbpi0/LinkDef.h
+++ bnbpi0/LinkDef.h
@@ -9,6 +9,10 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class sample+;
+//#pragma link C++ class sample+;
+#pragma link C++ class larlite::neutralpion+;
+#pragma link C++ class bottleopener+;
 //ADD_NEW_CLASS ... do not change this line
 #endif
+
+
